bee7386f53550e8e07310240c86c1baee16f5deb
Onrois <info@onrois.com>
2025-01-06 14:45:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    host: true
+  },
+  server: {
+    port: 3002,
+    host: true,
+    watch: {
+      usePolling: true
+    }
+  }
 })
